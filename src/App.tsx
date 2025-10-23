df6df44c42037d8b3887f0fbc2917775aabedbd5
Grkn02 <gurgulbahar285@gmail.com>
2025-10-23 22:25:55 +0300

    Oyunda kullanılcak olan tüm componentlerin belirlenmesi dosya hiyerarşisi kurularak düzenlenlenmesi.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
 import React, { useState } from 'react';
-import ImageCard from './components/ImageCard/ImageCard';
-
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-
-
 
 
 function App() {
